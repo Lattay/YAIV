da7ca8e2e383254e4210e6447d9c343e7746d072
mgamigo <mgamigo@users.noreply.github.com>
2025-04-29 13:43:39 +0300

    change of file structure

--- Tutorial/data/calcSettings.sh
+++ Tutorial/data/calcSettings.sh
@@ -3,3 +3,4 @@ SOFTWARE=${HOME}/Software
 export PSEUDOS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/pseudos
 export NPROCS=2
 export QE_PATH=$SOFTWARE/qe-7.3/bin
+export VASP_PATH=$SOFTWARE/VASP/vasp.6.4.1/bin
