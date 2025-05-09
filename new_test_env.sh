66b20ad2856f749dc6cd5992a12a6cc2cb6eb1c7
mgamigo <mgamigo@users.noreply.github.com>
2025-05-09 08:56:44 +0300

    Move Tutorial to dev

--- new_test_env.sh
+++ new_test_env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Usefull script to create test_enviroments and check installation
-ENV=qgt-dev
-J_ENV=QGT-dev
+ENV=yaiv-test
 DIR=`pwd`
 cd ~/Software/enviroments
 #jupyter kernelspec uninstall test_env
@@ -9,11 +8,10 @@ rm -r $ENV
 /usr/bin/python3.10 -m venv $ENV
 source $ENV/bin/activate
 pip install --upgrade pip
-#pip install ipykernel==6.17.1
 pip install ipykernel
-python -m ipykernel install --user --name=$J_ENV
+#python -m ipykernel install --user --name=$ENV
 #jupyter kernelspec list
 cd $DIR
 #Autoinstall YAIV
-#pip install -e ./ | tee install.log
+pip install -e ./ | tee install.log
 #pip install .
