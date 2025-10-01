e9261957c3885fe279170815d5f4b48f0267e29f
mgamigo <mgamigo@users.noreply.github.com>
2025-10-01 13:54:37 +0300

    Fix test_spectrum.py

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -22,7 +22,7 @@ FILES = [
     ("qe/results/Si.dyn1", "qe_dyn"),
     ("qe/results/Si.freq", "qe_freq_out"),
     ("qe/results/Si.proj.pwo", "qe_proj_out"),
-    ("qe/tmp/Si.xml", "qe_xml"),
+    ("qe/results/Si.xml", "qe_xml"),
     ("vasp/RESULTS/PROCAR", "procar"),
     ("vasp/RESULTS/OUTCAR", "outcar"),
     ("vasp/RESULTS/EIGENVAL", "eigenval"),
