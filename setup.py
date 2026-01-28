5baf21cfa23809d23b6f8b2e630462def78b3d1f
mgamigo <mgamigo@users.noreply.github.com>
2026-01-28 13:34:16 +0200

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mgamigo/YAIV",
-    packages=find_packages(exclude=("tests", "docs")),
+    packages=setuptools.find_packages(exclude=("tests", "docs")),
     install_requires=[
         "ipympl",
         "pint",
