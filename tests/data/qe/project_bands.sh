74e1bf31a2a61e4fa762726b2fc5ffa527b5abb3
mgamigo <mgamigo@users.noreply.github.com>
2026-02-26 22:23:09 +0200

    Minor change

--- tests/data/qe/project_bands.sh
+++ tests/data/qe/project_bands.sh
@@ -24,7 +24,7 @@ EOF
 
 echo "running the projection calculation"
 mpiexec -np $NPROCS $QE_PATH/projwfc.x <$NAME.proj.pwi >$NAME.proj.pwo
-rm pdos.data*
+rm pdos*
 rm proj.dat*
 rm input_tmp.in
 echo "done"
